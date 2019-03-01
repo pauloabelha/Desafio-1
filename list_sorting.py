752c09a8dd9a42fbeb1003f52c4f2a448c19704e
Igão Guru <47335482+IgaoGuru@users.noreply.github.com>
2019-02-28 21:49:50 -0300

    Add files via upload

--- list_sorting.py
+++ list_sorting.py
@@ -1,4 +1,4 @@
-input_list = [2, 1, 5, 3, 6, 4, 7]
+input_list = [1, 6, 4, 22, 12, 24, 10, 7]
 # Sorting code
 def sorter(lista):
   new_list = []
@@ -8,32 +8,49 @@ def sorter(lista):
   posy2 = 0
   x = lista[0 + posx2]
   y = lista[1 + posy2]
-  print posx2
-  print posy2
+  counter = 0
+  counter2 = 0
 
-  for i in range((len(lista))):
-    x = lista[0 + posx2]
-    y = lista[1 + posy2]
-    print "go"
-    if x > y:
-      inverter(x, y, lista, new_list)
-      posx2 = posx2 + 1
-      posy2 = posy2 + 1
-      continue
-    elif x < y:
-      posx2 += 1
-      posy2 += 1
-      continue
-  print posx2
-  print posy2
-  print new_list
+  while counter == False:
+    print "9: counter2 == %s, posy2 == %s" % (counter2, posy2)
+    if posy2 >= (len(lista) - 1):
+        print "1: started reseting"
+        posx2 = 0
+        posy2 = 0
+        x = 0
+        y = 1
+        counter2 += 1
+        print "2: %s %s %s %s" % (x, y, posx2, posy2)
+        if counter2 == 10:
+            counter = True
+        else:
+            counter = False
+    else:
+      print "3: standard"
+      x = lista[0 + posx2]
+      y = lista[1 + posy2]
+      print "4: %s, %s" % (x, y)
+      if x > y:
+        print "5: %s" % (lista)
+        inverter(x, y, lista)
+        posx2 = posx2 + 1
+        posy2 = posy2 + 1
+        print "6: x > y == %s" % (lista)
+        continue
+      elif x < y:
+        print "7: x < y"
+        posx2 += 1
+        posy2 += 1
+        print "8: list == %s" % (lista)
+        continue
+  print lista
 
 
-def inverter(x, y, lista, new_list):
+def inverter(x, y, lista):
   posx = lista.index(x)
   posy = lista.index(y)
-  new_list.insert(posx, y)
-  new_list.insert(posy, x)
+  lista.remove(x)
+  lista.insert(posy, x)
 
 sorter(input_list)
 
